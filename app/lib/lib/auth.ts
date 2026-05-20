bafe9ac4bdef4a713d5eae5121815ffdc6cd35b2
addie brock <addiebrock246@gmail.com>
2026-05-20 19:27:01 +0530

    Delete app directory

--- app/lib/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export function signToken(username: string) {
-  return jwt.sign({ username }, process.env.JWT_SECRET!, { expiresIn: '7d' });
-}
-
-export function verifyToken(token: string) {
-  return jwt.verify(token, process.env.JWT_SECRET!) as { username: string };
-}
